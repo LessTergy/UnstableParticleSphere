ea929f49c179ed2bdb28b9895fd226b80f33f0d8
Stanislav Zholobov <lesstergy@gmail.com>
2018-03-18 19:41:44 +0300

    Replace change color logic with ChangingColor script

--- Assets/Scripts/UnstableSphere.cs
+++ Assets/Scripts/UnstableSphere.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using Lesstergy.Colors;
 
 [RequireComponent(typeof(ParticleSystem))]
 public class UnstableSphere : MonoBehaviour {
@@ -6,29 +7,24 @@ public class UnstableSphere : MonoBehaviour {
     [Header("Particle Preferences")]
     public int particleCount = 1000;
     public float particleSize = 0.1f;
-
-
+    
     [Space(10)]
     [Header("Sphere Preferences")]
     public float sphereRadius = 5f;
+    public ChangingColor changingColor;
 
     [Space(10)]
     [Header("Physics Preferences")]
     public float particleMass = 0.01f;
 
-    [Range(1f, 85f)]
-    public float deltaGravityAngleForChange = 20f;
+    [Range(1f, 180f)]
+    public float gravityAngle = 20f;
+    [Range(1f, 200f)]
+    public float gravityForceRatio = 1f;
     public Vector3 accelerateDirection = new Vector3(0f, 10f, 0f);
 
-    [Range(1f, 200f)]
-    public float forceRatio = 1f;
 
-    public Color upColor;
-    public Color downColor;
-
-    public float timeColorDelay = 1f;
-    private TimeSince timeColorSince = new TimeSince();
-
+    //private data
     private Vector3 circleCenter;
 
     private new ParticleSystem particleSystem;
@@ -41,12 +37,6 @@ public class UnstableSphere : MonoBehaviour {
         InitParticles();
 
         circleCenter = transform.position;
-        Cursor.visible = false;
-
-        lastUpColor = upColor;
-        lastDownColor = downColor;
-        nextUpColor = upColor;
-        nextDownColor = downColor;
 	}
 
     private void OnValidate() {
@@ -66,7 +56,6 @@ public class UnstableSphere : MonoBehaviour {
             Vector3 randomPoint = Random.insideUnitSphere;
             Vector3 startPosition = randomPoint * sphereRadius + transform.position;
             particle.position = startPosition;
-            data.startPosition = startPosition;
 
             particle.startColor = new Color(1, 1, 1);
             particle.startSize = particleSize;
@@ -82,8 +71,6 @@ public class UnstableSphere : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Space)) {
             InitParticles();
         }
-
-        UpdateColors();
 	}
 
     private void FixedUpdate() {
@@ -91,6 +78,8 @@ public class UnstableSphere : MonoBehaviour {
     }
 
     private void UpdateParticles() {
+        changingColor.Update();
+
         for (int i = 0; i < particleCount; i++) {
             //Get
             ParticleSystem.Particle particle = particles[i];
@@ -98,12 +87,12 @@ public class UnstableSphere : MonoBehaviour {
 
             #region Change
             //Gravity
-            Vector3 direction = circleCenter - particle.position;
-            Vector3 gravityForce = direction.normalized * forceRatio * particleMass;
+            Vector3 gravityDirection = circleCenter - particle.position;
+            Vector3 gravityForce = gravityDirection.normalized * gravityForceRatio * particleMass;
 
             float deltaGravityAngle = Vector3.Angle(gravityForce, data.prevGravityForce);
             
-            if (deltaGravityAngle > deltaGravityAngleForChange) {
+            if (deltaGravityAngle > gravityAngle) {
                 data.acceleration = accelerateDirection;
             } else {
                 data.acceleration += gravityForce;
@@ -112,8 +101,7 @@ public class UnstableSphere : MonoBehaviour {
             data.prevGravityForce = gravityForce;
 
             //Color
-            Color color = Color.Lerp(downColor, upColor, Mathl.Map(particle.position.x, -20, 20, 0, 1f));
-            particle.startColor = color;
+            particle.startColor = changingColor.value;
 
             //Acceleration
             particle.position += data.acceleration * Time.deltaTime;
@@ -127,33 +115,7 @@ public class UnstableSphere : MonoBehaviour {
         particleSystem.SetParticles(particles, particleCount);
     }
 
-
-    Color lastUpColor;
-    Color lastDownColor;
-
-    Color nextUpColor;
-    Color nextDownColor;
-    private void UpdateColors() {
-        float delta = timeColorSince.delta;
-
-        float map = Mathl.Map(delta, 0f, timeColorDelay, 0f, 1f);
-
-        upColor = Color.Lerp(lastUpColor, nextUpColor, map);
-        downColor = Color.Lerp(lastDownColor, nextDownColor, map);
-
-        if (delta > timeColorDelay) {
-            lastUpColor = upColor;
-            lastDownColor = downColor;
-
-            nextUpColor = new Color(Random.value, Random.value, Random.value);
-            nextDownColor = new Color(Random.value, Random.value, Random.value);
-            
-            timeColorSince.Fixate();
-        }
-    }
-
     public struct ParticleData {
-        public Vector3 startPosition;
         public Vector3 acceleration;
         public Vector3 prevGravityForce;
     }
